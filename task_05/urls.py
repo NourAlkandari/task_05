7bb2da0c7f3677224ab50dee0f276f5e57d27420
Nour Alkandari <nouralkandari@Nours-MacBook-Pro.local>
2019-01-23 20:28:52 +0300

    adding one restaurant

--- task_05/urls.py
+++ task_05/urls.py
@@ -17,9 +17,10 @@ from django.contrib import admin
 from django.urls import path
 from restaurants import views
 
+
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('hello/',views.welcome ,name='hello-world'),
     path('restaurants/list/',views.restaurant_list ,name='restaurant-list'),
-    path('restaurants/detail/',views.restaurant_detail ,name='restaurant-detail'),
+    path('restaurants/detail/<int:restaurant_id>',views.restaurant_detail ,name='restaurant-detail'),
 ]
